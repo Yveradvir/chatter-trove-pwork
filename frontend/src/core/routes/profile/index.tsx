58129d7b5c2527ece174aa627efdc5ccf3091d13
Yveradvir <yveradvirr@gmail.com>
2024-11-06 21:00:53 +0200

    profile

--- frontend/src/core/routes/profile/index.tsx
+++ frontend/src/core/routes/profile/index.tsx
@@ -5,11 +5,16 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import UserInfo from "./components/userInfo";
+import { Tab, TabGroup, TabList, TabPanels } from "@headlessui/react";
 
 const ProfilePage = () => {
     const dispatch = useDispatch();
     const { user_id } = useParams<{ user_id: string }>();
     const userId = parseInt(user_id as string, 10);
+
+    const categories = [
+        {name: "Memberships", component: <></>},
+    ]
 
     useEffect(() => {
         if (isNaN(userId)) {
@@ -30,6 +35,21 @@ const ProfilePage = () => {
                     <h1 className="text-3xl font-bold">
                         Look for information here
                     </h1>
+
+                    <TabGroup>
+                        <TabList className="flex gap-4">
+                            {categories.map(({ name }) => (
+                                <Tab
+                                    key={name}
+                                    className="rounded-full py-1 px-3 text-sm/6 font-semibold text-white focus:outline-none data-[selected]:bg-white/10 data-[hover]:bg-white/5 data-[selected]:data-[hover]:bg-white/10 data-[focus]:outline-1 data-[focus]:outline-white"
+                                >
+                                    {name}
+                                </Tab>
+                            ))}
+                        </TabList>
+                        <TabPanels className="mt-3">
+                        </TabPanels>
+                    </TabGroup>
                 </main>
             </div>
         </Layout>
